f332964da00f04562102b8de18ea6206cbaaaea0
shy_azusa <shy.azusa@gmail.com>
2019-02-01 15:13:45 +0900

    Add back (#4)
    * Add back
    
    * Add webpack builder
    
    * Build!!
    
    * Add back in .js
    
    * Build!!

--- src/main.js
+++ src/main.js
@@ -6,5 +6,7 @@ makeCommand = () => {
   const pr_id = $('#pr_id').val();
   const branch_name = $('#branch_name').val();
   const ans = "$ git fetch origin pull/" + pr_id + "/head:" + branch_name + " && git checkout " + branch_name;
+  const back = "$ git checkout - && git branch -D " + branch_name;
   $('#ans').html(ans);
+  $('#back').html(back);
 }
